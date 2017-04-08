42fef84fddabaac476d45718887d1da573f38218
Parthasarathy Ramanujam <zincoshine>
2017-04-08 23:23:46 +0530

    Added smart contract file with basic contract

---
+++ solidity-smart-contract/Artefact.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.0;
+
+contract Artefact {
+  address owner;
+  string ARTEFACT_NAME;
+  string ARTEFACT_SIGNATURE;
+  //we have assumed a simple commission structure for this contract
+  address commReceiver;
+  uint commAmount;
+
+  function Aretfact(string _name,string _hash,address _receiver,uint _commAmount) {
+    owner = msg.sender;
+    ARTEFACT_NAME = _name;
+    ARTEFACT_SIGNATURE = _hash;
+    commReceiver = _receiver;
+    commAmount = _commAmount;
+  }
+
+  function transferOwner(address _newOwner) {
+    if(msg.sender != owner) {
+      throw;
+    }
+    owner = _newOwner;
+  }
+
+  function payout() payable returns (bool){
+    if(msg.sender.balance != commAmount) {
+      throw;
+    }
+    return commReceiver.send(msg.value);
+  }
+}
